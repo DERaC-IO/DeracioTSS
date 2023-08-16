7f504064f124a08091a963e697bc4be3bcc8ea7a
Tetsuya Saito <79015182+ts0924@users.noreply.github.com>
2023-08-17 02:38:20 +0900

    Update tss.py

--- tss.py
+++ tss.py
@@ -131,8 +131,10 @@ if __name__ == "__main__":
 </head>
 <body>
 {% block body %}
-<div class="uk-container">
-
+<div class="uk-margin-top uk-text-center">
+    <img src="https://github.com/DERaC-IO/DeracioTSS/blob/2a43aac2a5138902f83d24b38a367b7366aa8548/misc/deractss_logo.png">
+    <div class="uk-h1">Congratulations!</div>
+    <div class="uk-text-muted">Tornado is now working!</div>
 </div>
 {% end %}
 </body>
