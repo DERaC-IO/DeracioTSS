23f56fd70feff35f4f3629cfcea1694ce57bdd59
Tetsuya Saito <79015182+ts0924@users.noreply.github.com>
2023-08-22 11:54:51 +0900

    Create settings.py

---
+++ tornado_project/settings.py
@@ -0,0 +1,7 @@
+# Settings generated from JSON files (identified by .json) in settings.d folders
+
+config = {
+	"template": <TEMPLATE_FOLDER_NAME>,
+	"static": <STATIC_FOLDER_NAME>,
+	"port": <PORT_NUMBER>,
+}
